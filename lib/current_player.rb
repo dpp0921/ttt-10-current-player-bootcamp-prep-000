fd0d534810e838cd90e1f8d989b6dc608b038344
Chen Ye <dpp0921@gmail.com>
2018-01-19 00:41:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
 def turn_count(board)
   count = 1
   board.each do |turn|
-    current_player(board)
     count += 1
   end
 end
 def current_player(board)
+  turn_count(board)
   if count % 2 == 0
     current_player = "O"
   else
